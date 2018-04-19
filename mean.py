12de34a6da21f188e896dde5a21b43b5a976989d
Colette Gallagher <10009693@gmit.ie>
2018-04-19 11:02:54 +0100

    Update mean.py
    Updated Mean.py to calculate all columns in the Iris dataset

--- mean.py
+++ mean.py
@@ -1,5 +1,5 @@
 # Colette Gallagher, 19th April 2018
-# Project - Iris
+# Iris Project
 # To find the mean of each column of the Iris Data Set
 
 # Run ipython program in python
@@ -14,16 +14,29 @@ import numpy
 
 data = numpy.genfromtxt('data/iris.csv',delimiter = ',')
 
-# Specify the column of data to be read
+# Specify the columns of data to be read
 
 firstcol = data[:,0]
+secondcol = data[:,1]
+thirdcol = data[:,2]
+fourthcol = data[:,3]
 
-# Calculate the mean of the first column
+# Calculate the mean of the four columns
 
 meanfirstcol=numpy.mean(data[:,0])
+meansecondcol=numpy.mean(data[:,1])
+meanthirdcol=numpy.mean(data[:,2])
+meanfourthcol=numpy.mean(data[:,3])
 
-# Print answer
+# Print answers
 
-meanfirstcol
+print('The mean of the Sepal Length is', meanfirstcol)
+print('The mean of the Sepal Width is', meansecondcol)
+print('The mean of the Petal Length is' meanthirdcol)
+print('The mean of the Petal Width is', meanfourthcol)
 
-# The mean of the first column is 5.8433
+# Answers - The Mean of the Columns in the Iris Dataset
+# The mean of the Sepal Length is 5.84
+# The mean of the Sepal Width is 3.054
+# The mean of the Petal Length is 3.76
+# The mean of the Petal Width is 1.20
